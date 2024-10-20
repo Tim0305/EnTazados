e0519d9094af4fb4bbb573943c473c0bf74649e1
Karen <navarreteruizkarensofia@gmail.com>
2024-10-19 22:05:28 -0600

    Ref: Adicion de las rutas y vistas

--- entazados/src/app/app.routes.ts
+++ entazados/src/app/app.routes.ts
@@ -1,27 +1,17 @@
 import { Routes } from '@angular/router';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-frecuentes.component';
+import { AvisoDePrivacidadComponent } from './aviso-de-privacidad/aviso-de-privacidad.component';
+import { TerminosYCondicionesComponent } from './terminos-ycondiciones/terminos-ycondiciones.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
-  {
-    path: 'catalogo',
-    loadComponent: () =>
-      import('./catalogo/catalogo.component').then(
-        (m) => m.CatalogoComponent
-      ),
-  },
-  {
-    path: 'preguntas-frecuentes',
-    loadComponent: () =>
-      import('./preguntas-frecuentes/preguntas-frecuentes.component').then(
-        (m) => m.PreguntasFrecuentesComponent
-      ),
-  },
-  {
-    path: '',
-    redirectTo: 'catalogo',  // Ruta por defecto redirige al catálogo
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    redirectTo: 'catalogo',  // Cualquier ruta desconocida redirige al catálogo
-  }
+   { path: 'catalogo', component: CatalogoComponent },
+   { path: 'preguntas-frecuentes', component: PreguntasFrecuentesComponent },
+   { path: 'aviso-de-privacidad', component: AvisoDePrivacidadComponent },
+   { path: 'terminos-ycondiciones', component: TerminosYCondicionesComponent },
+   { path: 'pedidos', component: PedidosComponent },
+   { path: 'contacto', component: ContactoComponent },
+   { path: '', redirectTo: '/catalogo', pathMatch: 'full' }  // Redirige por defecto al catálogo
 ];
