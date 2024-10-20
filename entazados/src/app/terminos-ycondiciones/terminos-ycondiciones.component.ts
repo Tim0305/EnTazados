8c6b07268c61ba88b6c811f1f6bcbf0054c998db
Paioker44 <paolin4816@gmail.com>
2024-10-19 20:44:52 -0600

    se añadi el aviso de privacidad, contacto y terminos y condiciones

---
+++ entazados/src/app/terminos-ycondiciones/terminos-ycondiciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terminos-ycondiciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './terminos-ycondiciones.component.html',
+  styleUrl: './terminos-ycondiciones.component.css'
+})
+export class TerminosYCondicionesComponent {
+
+}
