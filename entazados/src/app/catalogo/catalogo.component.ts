cd51db0c342aa54ecfd222a1f51fcb9e3b9d7ab8
Karen <navarreteruizkarensofia@gmail.com>
2024-10-20 18:38:33 -0600

    aesfrwge

--- entazados/src/app/catalogo/catalogo.component.ts
+++ entazados/src/app/catalogo/catalogo.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../services/producto.service';
 
 @Component({
-  selector: 'app-catalogo',
-  standalone: true,
-  imports: [],
-  templateUrl: './catalogo.component.html',
-  styleUrl: './catalogo.component.css'
+   selector: 'app-catalogo',
+   templateUrl: './catalogo.component.html',
+   styleUrls: ['./catalogo.component.css']
 })
-export class CatalogoComponent {
+export class CatalogoComponent implements OnInit {
+   public productos: any[] = [];
 
+   constructor(private productService: ProductoService) { }
+
+   ngOnInit(): void {
+      // Llamar al servicio para obtener los productos desde la API
+      this.productService.getProducts().subscribe(
+         (response) => {
+            this.productos = response; // Asignar la respuesta de la API a la variable 'productos'
+         },
+         (error) => {
+            console.error('Error al obtener los productos:', error); // Manejo de errores
+         }
+      );
+   }
 }
