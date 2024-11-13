66f1b6fb4b6922e6fdf1f30655c284c3f888e348
Timoteo <tabtim1015@gmail.com>
2024-11-13 17:20:44 -0600

    Feat: Ya funciona el registro de usuario

--- entazados/src/app/sign-up/sign-up.component.ts
+++ entazados/src/app/sign-up/sign-up.component.ts
@@ -3,48 +3,49 @@ import { Usuario } from '../../models/Usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
-
 export class SignUpComponent {
-   private apiURL = 'http://localhost:8080/usuarios'; // URL de la API
+  private apiURL = 'http://localhost:8080/usuarios'; // URL de la API
 
-   // Propiedades para el nuevo producto
-   usuarios: any = {
-      nombre: '',
-      apellidos: '',
-      correo: '',
-      direccion: '',
-      password: ''
-   };
+  // Propiedades para el nuevo producto
+  usuario: Usuario = {
+    id: 0,
+    nombre: '',
+    apellidos: '',
+    correo: '',
+    direccion: '',
+    rol: 0,
+    password: '',
+  };
 
-   constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-   onClickAgregarUsuario() {
-      console.log('Datos enviados:', this.usuarios);
-      this.http.post(this.apiURL, this.usuarios).subscribe(
-         () => {
-            alert('Usuario registrado exitosamente');
-            // Aquí puedes limpiar el formulario o mostrar un mensaje de éxito si es necesario
-            this.usuarios = {
-               nombre: '',
-               apellidos: '',
-               correo: '',
-               direccion: '',
-               rol: 0,
-            };
-         },
-         (error) => {
-            console.error('Error al registrar el usuario:', error);
-            alert("Error al registrar el usuario");
-         }
-      );
-   }
+  onClickAgregarUsuario() {
+    // Realizar una solicitud POST con el objeto `product` al API
+    this.http.post(this.apiURL, this.usuario).subscribe(
+      () => {
+        alert('Usuario registrado exitosamente');
+        // Aquí puedes limpiar el formulario o mostrar un mensaje de éxito si es necesario
+        this.usuario = {
+          id: 0,
+          nombre: '',
+          apellidos: '',
+          correo: '',
+          direccion: '',
+          rol: 0,
+          password: '',
+        };
+      },
+      (error) => {
+        console.error('Error al registrar el usuario:', error);
+        alert('Error al registrar el usuario');
+      }
+    );
+  }
 }
-
