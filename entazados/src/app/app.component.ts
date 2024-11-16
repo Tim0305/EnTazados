3030380e1764005163cbc2e296fccba0baa182de
Karen <navarreteruizkarensofia@gmail.com>
2024-11-15 19:46:48 -0600

    boton de agregar producto

--- entazados/src/app/app.component.ts
+++ entazados/src/app/app.component.ts
@@ -4,29 +4,29 @@ import { UsuarioService } from '../services/UsuarioService/usuario.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+   selector: 'app-root',
+   standalone: true,
+   imports: [RouterOutlet, RouterLink, CommonModule],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'entazados';
+   title = 'entazados';
 
-  constructor(private userService: UsuarioService) {}
+   constructor(private userService: UsuarioService) { }
 
-  isAdmin() {
-    if (this.userService.getRol() === 1) return true;
-    return false;
-  }
+   isAdmin() {
+      if (this.userService.getRol() === 1) return true;
+      return false;
+   }
 
-  isUser() {
-    if (this.userService.getRol() === 2) return true;
-    return false;
-  }
+   isUser() {
+      if (this.userService.getRol() === 2) return true;
+      return false;
+   }
 
-  isLoggedIn() {
-    if (this.userService.getRol() !== 0) return true;
-    return false;
-  }
+   isLoggedIn() {
+      if (this.userService.getRol() !== 0) return true;
+      return false;
+   }
 }
